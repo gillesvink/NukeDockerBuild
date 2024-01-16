645ab0738aa6bab0df7990bd4fafe095966e1fd0
Gilles Vink <gilles@vinkvfx.com>
2024-01-16 13:09:59 +0100

    Add retriever function and filter to keep latest tags

--- table_retriever/worker/retriever.py
+++ table_retriever/worker/retriever.py
@@ -2,4 +2,61 @@
 
 @maintainer: Gilles Vink
 """
+import json
+import os
+import re
 
+import requests
+
+from table_retriever.datamodel.constants import GithubData
+
+
+def _filter_tags(tags: set[str]) -> list[str]:
+    """Filter provided tags to keep only the highest versions.
+
+    Args:
+        tags: tags to process and remove duplicats.
+
+    Returns:
+        filtered list of tags.
+    """
+    collected_tags = set()
+    for tag in tags:
+        if "latest" in tag:
+            collected_tags.add(tag)
+
+        platform, _ = tag.rsplit("-", 1)
+        if not any(
+            platform in collected_tag for collected_tag in collected_tags
+        ):
+            collected_tags.add(tag)
+
+        regex_match = rf"{platform}-((\d).(\d))"
+        same_target_tags = [tag for tag in tags if re.match(regex_match, tag)]
+        same_target_tags.sort(reverse=True)
+        if same_target_tags[0] != tag:
+            continue
+
+        collected_tags.add(tag)
+
+    return collected_tags
+
+
+def _retrieve_tags() -> set[str]:
+    """Retrieve data from GHCR containing all tags."""
+    authorization_header = {"Authorization": f"Bearer {os.environ['TOKEN']}"}
+    api_url = f"{GithubData.GHCR_API.value}/tags/list"
+    requested_data: requests.Response = requests.get(
+        api_url, headers=authorization_header, timeout=3
+    )
+    if requested_data.status_code != 200:
+        msg = "No data found on server."
+        raise RetrieveError(msg)
+
+    data = json.loads(requested_data.json())
+    tags = data.get("tags")
+    return _filter_tags(tags)
+
+
+class RetrieveError(Exception):
+    """Error to raise when something went wrong during retrieving of data."""
--- tests/table_retriever/test_retriever.py
+++ tests/table_retriever/test_retriever.py
@@ -2,3 +2,152 @@
 
 @maintainer: Gilles Vink
 """
+import contextlib
+import json
+import os
+from unittest.mock import MagicMock, patch
+
+import pytest
+import requests
+
+from table_retriever.datamodel.constants import GithubData
+from table_retriever.worker.retriever import RetrieveError, _retrieve_tags
+
+
+@pytest.fixture(autouse=True)
+def _set_token() -> None:
+    """Set test token"""
+    os.environ["TOKEN"] = "123"
+
+
+class TestRetrieveTags:
+    """Tests related to the retrieve function."""
+
+    def test__retrieve_tags(self) -> None:
+        """Test to call the correct functions."""
+        os_mock = MagicMock(spec=os)
+        os_mock.environ.__getitem__.return_value = "123"
+        with patch(
+            "table_retriever.worker.retriever.requests"
+        ) as requests_mock, patch(
+            "table_retriever.worker.retriever.os", os_mock
+        ), contextlib.suppress(
+            RetrieveError
+        ):
+            _retrieve_tags()
+        expected_headers = {"Authorization": "Bearer 123"}
+        api_url = f"{GithubData.GHCR_API.value}/tags/list"
+        requests_mock.get.assert_called_once_with(
+            api_url, headers=expected_headers, timeout=3
+        )
+        os_mock.environ.__getitem__.assert_called_once_with("TOKEN")
+
+    def test__retrieve_tags_no_data_found(self) -> None:
+        """Test to raise a RetrieveError if status code is not 200"""
+        requests_mock = MagicMock(spec=requests)
+        response_mock = MagicMock(spec=requests.Response)
+        response_mock.status_code = 404
+        requests_mock.get.return_value = response_mock
+        with patch(
+            "table_retriever.worker.retriever.requests", requests_mock
+        ), pytest.raises(RetrieveError, match="No data found on server."):
+            _retrieve_tags()
+
+    @pytest.mark.parametrize(
+        ("test_data", "expected_data"),
+        [
+            (
+                {
+                    "name": "not important",
+                    "tags": [
+                        "13.0-linux-latest",
+                        "13.0-linux-1.0",
+                    ],
+                },
+                {"13.0-linux-latest", "13.0-linux-1.0"},
+            ),
+            (
+                {
+                    "name": "not important",
+                    "tags": [
+                        "13.0-linux-latest",
+                        "13.0-linux-1.0",
+                        "13.0-linux-1.1",
+                    ],
+                },
+                {"13.0-linux-latest", "13.0-linux-1.1"},
+            ),
+            (
+                {
+                    "name": "not important",
+                    "tags": [
+                        "13.0-windows-latest",
+                        "13.0-windows-1.0",
+                        "13.0-linux-latest",
+                        "13.0-linux-1.0",
+                        "13.0-linux-1.1",
+                    ],
+                },
+                {
+                    "13.0-windows-latest",
+                    "13.0-windows-1.0",
+                    "13.0-linux-latest",
+                    "13.0-linux-1.1",
+                },
+            ),
+            (
+                {
+                    "name": "not important",
+                    "tags": [
+                        "13.0-windows-latest",
+                        "13.0-windows-1.0",
+                        "14.0-linux-latest",
+                        "14.0-linux-1.0",
+                        "14.0-linux-1.1",
+                    ],
+                },
+                {
+                    "13.0-windows-latest",
+                    "13.0-windows-1.0",
+                    "14.0-linux-latest",
+                    "14.0-linux-1.1",
+                },
+            ),
+            (
+                {
+                    "name": "not important",
+                    "tags": [
+                        "13.0-windows-latest",
+                        "13.0-windows-1.0",
+                        "14.0-linux-latest",
+                        "14.0-linux-1.0",
+                        "14.0-linux-1.1",
+                        "14.0-linux-slim-latest",
+                        "14.0-linux-slim-1.0",
+                        "14.0-linux-slim-1.1",
+                    ],
+                },
+                {
+                    "13.0-windows-latest",
+                    "13.0-windows-1.0",
+                    "14.0-linux-latest",
+                    "14.0-linux-1.1",
+                    "14.0-linux-slim-latest",
+                    "14.0-linux-slim-1.1",
+                },
+            ),
+        ],
+    )
+    def test__return_value(self, test_data: dict, expected_data: dict) -> None:
+        """Test to return the expected data and strip redundant data."""
+        requests_mock = MagicMock(requests)
+        response_mock = MagicMock(spec=requests.Response)
+        response_mock.status_code = 200
+        response_mock.json.return_value = json.dumps(test_data)
+        requests_mock.get.return_value = response_mock
+
+        with patch("table_retriever.worker.retriever.requests", requests_mock):
+            retrieved_data = _retrieve_tags()
+
+        assert isinstance(retrieved_data, set)
+        assert retrieved_data == expected_data
