1c1b5299ea8786d93aaa3d0d9203624ddda070f7
Gilles Vink <gilles@vinkvfx.com>
2024-01-22 17:16:15 +0100

    Add global toolchain for Mac

---
+++ dependancies/macos/toolchain.cmake
@@ -0,0 +1,7 @@
+# Global toolchain file to setup CMake for OSXCross
+
+set(CMAKE_C_COMPILER o64-clang)
+set(CMAKE_CXX_COMPILER o64-clang++)
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_OSX_SYSROOT $ENV{MACOS_SDK})
+set(CMAKE_OSX_DEPLOYMENT_TARGET $ENV{DEPLOYMENT_TARGET})
