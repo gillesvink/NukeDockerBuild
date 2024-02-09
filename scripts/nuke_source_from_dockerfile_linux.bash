97ab992c05df3d520d5f7a26c0fc42103a9e34d2
Gilles Vink <gilles@vinkvfx.com>
2024-02-09 22:54:57 +0100

    Modify script for linux to keep all so files

--- scripts/nuke_source_from_dockerfile_linux.bash
+++ scripts/nuke_source_from_dockerfile_linux.bash
@@ -9,13 +9,14 @@ at the same time. Otherwise this could be easily done within the Dockerfile itse
 
 END_COMMENT
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <path_to_dockerfile> <target_folder>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <path_to_dockerfile> <minimized_target_folder> <full_target_folder>"
     exit 1
 fi
 
 dockerfile_path="$1"
 target_folder="$2"
+full_target_folder="$3"
 
 url=$(grep "LABEL 'com.nukedockerbuild.nuke_source'" "$dockerfile_path" | awk -F"=" '{print $2}' | tr -d "'")
 if [ -z "$url" ]; then
@@ -36,12 +37,14 @@ tar zxvf ${nuke_temp_files}/${filename} -C ${nuke_temp_files}
 echo "Remove compressed Nuke"
 rm ${nuke_temp_files}/${filename}
 
-echo "Install Nuke to ${target_folder}"
-${nuke_temp_files}/${filename%.*}.run --accept-foundry-eula --prefix=${target_folder} --exclude-subdir
+echo "Install Nuke to ${full_target_folder}"
+${nuke_temp_files}/${filename%.*}.run --accept-foundry-eula --prefix=${full_target_folder} --exclude-subdir
 
 echo "Keep only source files"
-mkdir ${target_folder}/tests
-cp -r ${target_folder}/Documentation/NDKExamples/examples/* ${target_folder}/tests
+mkdir ${full_target_folder}/tests
+cp -r ${full_target_folder}/Documentation/NDKExamples/examples/* ${full_target_folder}/tests
+find ${full_target_folder} -mindepth 1 -maxdepth 1 ! -name "tests" ! -name "cmake" ! -name "include" ! -name "*.h" ! -name "*.so" ! -name "source" -exec rm -rf {} \;
+cp -r ${full_target_folder}/* ${target_folder}
 find ${target_folder} -mindepth 1 -maxdepth 1 ! -name "tests" ! -name "cmake" ! -name "include" ! -name "*Fdk*" ! -name "*Fn*" ! -name "*Ndk*" ! -name "*DDI*" ! -name "source" -exec rm -rf {} \;
 
 echo "Clean nuke temp files"
