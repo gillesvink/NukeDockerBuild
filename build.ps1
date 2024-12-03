c201cf53693705cbe65e24c89d4c6ffc5c92fc57
Gilles Vink <gilles@vinkvfx.com>
2024-12-03 15:42:28 +0100

    Fix windows build

--- build.ps1
+++ build.ps1
@@ -6,20 +6,27 @@ $answer = Read-Host "Is the Nuke version correct: '$NUKEVERSION'. It can be for 
 
 if ($answer -match '^[Yy]$') {
     Write-Host "Continuing installation..."
-} else {
+}
+else {
     Write-Host "Please change the input"
     exit 1
 }
 
-Set-Location "dockerfiles\$NUKEVERSION\linux"
+Set-Location "dockerfiles\$NUKEVERSION\windows"
 Write-Host "Creating image for Nuke version: $NUKEVERSION"
 $SOURCES_DIR = "_nuke_sources"
 
+$originalPath = Get-Location
 New-Item -ItemType Directory -Force -Path $SOURCES_DIR
 
-& "../../../scripts/nuke_source_from_dockerfile_windows.ps1" "Dockerfile" $SOURCES_DIR
+$dockerfilePath = Resolve-Path "Dockerfile"
+$sourcesDirPath = Resolve-Path $SOURCES_DIR
+
+..\..\..\scripts\nuke_source_from_dockerfile.ps1 $dockerfilePath $sourcesDirPath
+
+Set-Location $originalPath
 
 docker buildx build `
-    -t "nukedockerbuild:$NUKEVERSION-linux-latest" `
+    -t "nukedockerbuild:$NUKEVERSION-windows" `
     --build-arg "NUKE_SOURCE_FILES=$SOURCES_DIR" `
     .
